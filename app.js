d2144bcdbcc4b2e748fc262cb4d8f311c6375e38
jcode3001 <jc0d3001@outlook.com>
2020-04-15 20:27:26 -0300

    usando o metodo de remocao

---
+++ app.js
@@ -0,0 +1,162 @@
+/**
+ * Váriaveis usadas durante o desenvolvimento
+ */
+var CARD_CONTAINER = document.getElementsByClassName('card-container')[0];
+var NOMES = ["Anderson", "Beatriz", "Caio", "Daniela", "Everton", "Fabiana", "Gabriel", "Hortencia", "Igor", "Joana"];
+var cards = [];
+// firebase e um objeto global 
+    // database() e um metodo de acesso ao meu real time database
+    // ref() e a referencia do caminho do banco 
+var ref = firebase.database().ref('card/');
+
+/**
+ * Botão para cria um card no card-contaier
+ */
+function criarCard() {
+    var card = {
+        nome: NOMES[Math.floor(Math.random() * NOMES.length - 1)],
+        idade: Math.floor(Math.random() * 22 + 18),
+        curtidas: 0
+    };
+    
+    // set() metodo que cria dados na url passada 
+    //metodo push e um metodo que cria um id unico, gerado em um hash de acordo com o date time que foi postado
+    ref.push(card).then(snapshot =>{
+        adicionaCardATela(card, snapshot.key);
+    });
+};
+
+/**
+ * Recebe a referencia do card e exclui do banco de dados
+ * @param {String} id Id do card
+ */
+function deletar(id) {
+    var card = document.getElementById(id);
+    //remove() remove o no com tds os nos filhos
+    ref.child(id).remove().then(() => {
+        card.remove();
+    });
+
+    //set(null) ao setar um no em nulo exclui esse no do firebase
+    //ref.child(id).set(null).then(() =>{    card.remove();});
+};
+
+/**
+ * Incrementa o numero de curtidas
+ * @param {String} id Id do card
+ */
+function curtir(id) {
+
+};
+
+/**
+ * Decrementa o numero de curtidas
+ * @param {String} id Id do card
+ */
+function descurtir(id) {
+
+};
+
+/**
+ * Espera o evento de que a DOM está pronta para executar algo
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    //once() retorna os dados lidos na url
+    //snapshot e um objeto retornado pela leitura
+    ref.once('value').then(snapshot => {
+        //acessa o no filho
+        //console.log('child',snapshot.child('-M4tGJNtOxt2mFrOqMZu').val());
+        //checa se existe algo no snapshot
+        //console.log('exists()', snapshot.exists());
+        // procura se existe um no filho passado pela url
+        //console.log('hasChild() nome', snapshot.hasChild('-M4tGJNtOxt2mFrOqMZu/nome'));
+        //console.log('hasChild() coomentario', snapshot.hasChild('-M4tGJNtOxt2mFrOqMZu/comentario'));
+        // procura se existe um filho no no
+        //console.log('hasChildren', snapshot.child('-M4tGJNtOxt2mFrOqMZu').hasChildren());
+        //retorna o numero de nos no snapshot
+        //console.log('numChildren()', snapshot.numChildren());
+        //a chave desse caminho
+        //console.log('chave', snapshot.key);
+        snapshot.forEach(value => {
+            //id
+            //console.log('chave', value.key);
+            adicionaCardATela(value.val(), value.key);
+        });
+    });
+});
+
+/**
+ * Adiciona card na tela
+ * @param {Object} informacao Objeto contendo dados do card
+ * @param {String} id UID do objeto inserido/consultado
+ */
+function adicionaCardATela(informacao, id) {
+    /**
+     * HEADER DO CARD
+     */
+    let header = document.createElement("h2");
+    header.innerText = informacao.nome;
+    header.classList.add('card-title');
+    // ===================================
+
+    /**
+     * CONTENT DO CARD
+     */
+    let content = document.createElement("p");
+    content.classList.add('card-text');
+    content.innerText = informacao.idade + ' anos.';
+    // ===================================
+
+    /**
+     * BOTÕES DO CARD
+     */
+    let inner = document.createElement("div");
+    inner.classList.add('row')
+    // Botão adicionar
+    let button_add = document.createElement("button");
+    button_add.classList.add('btn', 'btn-link', 'col-3');
+    button_add.setAttribute('onclick', "curtir('" + id + "')");
+    button_add.innerText = '+';
+    inner.appendChild(button_add);
+
+    // Contador de curtidas
+    let counter = document.createElement("span");
+    counter.innerHTML = informacao.curtidas;
+    counter.classList.add('col-3', 'text-center', 'count-number');
+    inner.appendChild(counter);
+
+    // Botão de subtrair
+    let button_sub = document.createElement("button");
+    button_sub.classList.add('btn', 'btn-link', 'col-3');
+    button_sub.setAttribute('onclick', "descurtir('" + id + "')");
+    button_sub.innerText = '-';
+    inner.appendChild(button_sub);
+    // ===================================
+
+    // Botão de excluir
+    let button_del = document.createElement("button");
+    button_del.classList.add('btn', 'btn-danger', 'col-3');
+    button_del.setAttribute('onclick', "deletar('" + id + "')");
+    button_del.innerText = 'x';
+    inner.appendChild(button_del);
+    // ===================================
+
+    /**
+     * CARD
+     */
+    let card = document.createElement("div");
+    card.classList.add('card');
+    card.id = id;
+    let card_body = document.createElement("div");
+    card_body.classList.add('card-body');
+    // ===================================
+
+    // popula card
+    card_body.appendChild(header);
+    card_body.appendChild(content);
+    card_body.appendChild(inner);
+    card.appendChild(card_body);
+
+    // insere no container
+    CARD_CONTAINER.appendChild(card);
+}
