28eed50216006ce0efd3d7a35805f1fd29358f62
GumblarZ <jc0d3001@outlook.com>
2020-05-30 11:03:19 -0300

    adicioando o auth

--- auth/js/app.js
+++ auth/js/app.js
@@ -3,3 +3,30 @@ function logout() {
         alert('Usuario deslogou');
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    //configuracoes do ui
+    const config = {
+        callbacks: {
+            //apos logar ele retorna uma autenticacao e uma  url
+            signInSucessWithAuthResult: function(authResult){
+                console.log('authrResult', authResult);
+                return false; 
+            }
+        },
+        signInOptions: [
+            //icones de login
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID
+            
+            
+        ],
+        signInFlow: 'popup'
+    };
+    //instanciamento do ui com os parametro id da div e as configuracoes salvas no config
+    ui.start('#firebaseui', config);
+});
